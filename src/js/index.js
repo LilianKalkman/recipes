da837570864b758a7955674475c47083c126e797
LilianKalkman <liliankalkman@hotmail.com>
2018-06-19 11:48:54 +0200

    get search data

--- src/js/index.js
+++ src/js/index.js
@@ -3,12 +3,24 @@ import axios from 'axios';
 async function getResults(query){
   const proxy = 'https://cors-anywhere.herokuapp.com/';
   const apikey = '4b943e043588f1fc8cd5a56a1c23e32c';
-  const result = await axios.get(`${proxy}http://food2fork.com/api/search?key=${apikey}&q=${query}`);
-  console.log(result);
+  try {
+    const result = await axios.get(`${proxy}http://food2fork.com/api/search?key=${apikey}&q=${query}`);
+    const recipes = result.data.recipes;
+    console.log(recipes);
+  } catch(error){
+    console.log(error);
+  }
 }
 
-getResults('pizza');
+function getResultsOldway(query){
+  const proxy = 'https://cors-anywhere.herokuapp.com/';
+  const apikey = '4b943e043588f1fc8cd5a56a1c23e32c';
+  axios.get(`${proxy}http://food2fork.com/api/search?key=${apikey}&q=${query}`)
+  .then(res => {
+    console.log(res.data.recipes);
+  })
+  .catch(error => console.log(error));
+}
 
-
-// 4b943e043588f1fc8cd5a56a1c23e32c   =API
-// http://food2fork.com/api/search    =Request url
+getResults('salmon');
+// getResultsOldway('salmon');
