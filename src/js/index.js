eac41a91d570647b985b974b3c59908959b1fd7a
LilianKalkman <liliankalkman@hotmail.com>
2018-06-19 12:31:26 +0200

    search model

--- src/js/index.js
+++ src/js/index.js
@@ -1,26 +1,6 @@
-import axios from 'axios';
+import Search from './models/Search';
 
-async function getResults(query){
-  const proxy = 'https://cors-anywhere.herokuapp.com/';
-  const apikey = '4b943e043588f1fc8cd5a56a1c23e32c';
-  try {
-    const result = await axios.get(`${proxy}http://food2fork.com/api/search?key=${apikey}&q=${query}`);
-    const recipes = result.data.recipes;
-    console.log(recipes);
-  } catch(error){
-    console.log(error);
-  }
-}
+const search1 = new Search('salmon');
+console.log(search1);
 
-function getResultsOldway(query){
-  const proxy = 'https://cors-anywhere.herokuapp.com/';
-  const apikey = '4b943e043588f1fc8cd5a56a1c23e32c';
-  axios.get(`${proxy}http://food2fork.com/api/search?key=${apikey}&q=${query}`)
-  .then(res => {
-    console.log(res.data.recipes);
-  })
-  .catch(error => console.log(error));
-}
-
-getResults('salmon');
-// getResultsOldway('salmon');
+search1.getResults();
---
+++ src/js/models/Search.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+export default class Search {
+  constructor(query){
+    this.query = query;
+  }
+
+  async getResults(){
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+    const apikey = '4b943e043588f1fc8cd5a56a1c23e32c';
+    try {
+      const result = await axios.get(`${proxy}http://food2fork.com/api/search?key=${apikey}&q=${this.query}`);
+      this.result = result.data.recipes;
+      console.log(this);
+    } catch(error){
+      console.log(error);
+    }
+  }
+}
+
+
+// function getResultsOldway(query){
+//   const proxy = 'https://cors-anywhere.herokuapp.com/';
+//   const apikey = '4b943e043588f1fc8cd5a56a1c23e32c';
+//   axios.get(`${proxy}http://food2fork.com/api/search?key=${apikey}&q=${query}`)
+//   .then(res => {
+//     console.log(res.data.recipes);
+//   })
+//   .catch(error => console.log(error));
+// }
