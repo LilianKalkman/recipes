ff951aae427f3d5b879eb71baebd0ff7568c9db8
LilianKalkman <liliankalkman@hotmail.com>
2018-06-26 13:25:25 +0200

    limit title

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -3,15 +3,29 @@ import {elements} from './elements';
 export const getInput = () => {
   const input = elements.searchInput.value;
   return input;
-}
+};
 
 export const clearInput = () => {
   elements.searchInput.value = '';
-}
+};
 
 export const clearResults = () => {
   elements.searchList.innerHTML = '';
-}
+};
+
+const limitRecipeTitle = (title, limit = 17) => {
+  const newtitle = [];
+  if(title.length > limit){
+    title.split(' ').reduce((total, current) => {
+      if(total + current.length <= limit) {
+        newtitle.push(current);
+      }
+      return total + current.length;
+    }, 0);
+    return `${newtitle.join(' ')}...`;
+  }
+  return title;
+};
 
 const renderRecipe = recipe => {
   const html = `
@@ -21,15 +35,15 @@ const renderRecipe = recipe => {
                 <img src="${recipe.image_url}" alt="Test">
             </figure>
             <div class="results__data">
-                <h4 class="results__name">${recipe.title}</h4>
+                <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
                 <p class="results__author">${recipe.publisher}</p>
             </div>
         </a>
     </li>
   `;
   elements.searchList.insertAdjacentHTML('beforeend', html);
-}
+};
 
 export const renderResults = recipes => {
   recipes.forEach(renderRecipe);
-}
+};
